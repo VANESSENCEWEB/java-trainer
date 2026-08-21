00c993042431de4363195f0bdb161bc85b0106ed
Vanessa Lima <200443247+VANESSENCEWEB@users.noreply.github.com>
2026-08-21 22:50:53 +0000

    adiciona tabuada de 1 a 10 com for

---
+++ 07-tabuada/Tabuada.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite um numero:");
+        int numero = leia.nextInt();
+
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
